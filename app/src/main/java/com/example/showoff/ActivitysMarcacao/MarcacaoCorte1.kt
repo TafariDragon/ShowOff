1ef3aa65ee5c6cbdd2a314f2d720e548010355ac
Tafari Vilanculos <tafarivilanculos@gmail.com>
2024-04-08 02:09:36 +0200

    Inicio da parte de Servicos

---
+++ app/src/main/java/com/example/showoff/ActivitysMarcacao/MarcacaoCorte1.kt
@@ -0,0 +1,12 @@
+package com.example.showoff.ActivitysMarcacao
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.showoff.R
+
+class MarcacaoCorte1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marcacao_corte1)
+    }
+}
--- app/src/main/java/com/example/showoff/Catalogo_Barbeiro.kt
+++ app/src/main/java/com/example/showoff/Catalogo_Barbeiro.kt
@@ -3,6 +3,7 @@ package com.example.showoff
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.showoff.ActivitysMarcacao.MarcacaoCorte1
 import com.example.showoff.databinding.ActivityCatalogoBarbeiroBinding
 
 class Catalogo_Barbeiro : AppCompatActivity() {
@@ -15,6 +16,10 @@ class Catalogo_Barbeiro : AppCompatActivity() {
         binding.iconBack.setOnClickListener {
             startActivity(Intent(this,MenuPrincipal::class.java))
 
+        }
+
+        binding.btnRealizarMarcacao.setOnClickListener {
+            startActivity(Intent(this,MarcacaoCorte1::class.java))
         }
     }
 }
