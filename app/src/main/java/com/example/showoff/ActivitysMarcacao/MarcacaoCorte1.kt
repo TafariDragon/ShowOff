a5cba285b293613e3ca45401dfd4028c294c98a3
Tafari Vilanculos <tafarivilanculos@gmail.com>
2024-04-11 23:52:52 +0200

    binding_MarcaraoBarbeiro

--- app/src/main/java/com/example/showoff/ActivitysMarcacao/MarcacaoCorte1.kt
+++ app/src/main/java/com/example/showoff/ActivitysMarcacao/MarcacaoCorte1.kt
@@ -1,12 +1,20 @@
 package com.example.showoff.ActivitysMarcacao
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.showoff.R
+import com.example.showoff.Catalogo_Barbeiro
+import com.example.showoff.databinding.ActivityMarcacaoCorte1Binding
 
 class MarcacaoCorte1 : AppCompatActivity() {
+    private lateinit var  binding : ActivityMarcacaoCorte1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_marcacao_corte1)
+        binding= ActivityMarcacaoCorte1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iconBack.setOnClickListener {
+            startActivity(Intent(this,Catalogo_Barbeiro::class.java))
+        }
     }
 }
