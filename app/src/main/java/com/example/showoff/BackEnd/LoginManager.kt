5c4b6d4a60572f5ba2e740783ac6fa1e570cd131
Tafari Vilanculos <tafarivilanculos@gmail.com>
2024-04-21 01:35:59 +0200

    Autenticacao

---
+++ app/src/main/java/com/example/showoff/BackEnd/LoginManager.kt
@@ -0,0 +1,52 @@
+package com.example.showoff.BackEnd
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+
+class LoginManager (private val context: Context) {
+
+    interface LoginListener {
+        fun onLoginSuccess()
+        fun onUserNotExist()
+        fun onLoginError(errorMessage: String)
+    }
+
+    fun fazerLogin(contacto: String, senha: String, listener: LoginListener) {
+        val urlConsulta = "http://192.168.43.49/showoffAPI/consulta.php?Contacto_C=$contacto&Senha_C=$senha"
+
+        val queue: RequestQueue = Volley.newRequestQueue(context)
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, urlConsulta,
+            null,
+            Response.Listener { response ->
+                try {
+                    if (response.has("error")) {
+                        val errorMessage = response.getString("error")
+                        listener.onLoginError("Erro: $errorMessage")
+                    } else {
+                        if (response.has("cliente_existe")) {
+                            listener.onLoginSuccess()
+                        } else {
+                            listener.onUserNotExist()
+                        }
+                    }
+                } catch (e: JSONException) {
+                    listener.onLoginError("Erro ao processar a resposta do servidor")
+                }
+            },
+            Response.ErrorListener { error ->
+                listener.onLoginError("Erro: ${error.message}")
+            }
+        )
+
+        queue.add(jsonObjectRequest)
+    }
+}
+
+
--- app/src/main/java/com/example/showoff/Login.kt
+++ app/src/main/java/com/example/showoff/Login.kt
@@ -1,12 +1,10 @@
 package com.example.showoff
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.showoff.BackEnd.LoginManager
 import com.example.showoff.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
@@ -14,25 +12,38 @@ class Login : AppCompatActivity() {
     private  lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fun hideKeyboard(view: View, event: MotionEvent) {
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(view.windowToken, 0)
-            }
-        }
-
         binding.btnCadastrar.setOnClickListener {
-            startActivity(Intent(this,CreateAccount::class.java))
+            startActivity(Intent(this, CreateAccount::class.java))
             finish()
         }
 
         binding.btnEntrar.setOnClickListener {
-            startActivity(Intent(this,MenuPrincipal::class.java))
-            finish()
+            val contacto = binding.txtLoginContactoUtilizador.text.toString().trim()
+            val senha = binding.txtSenhaLoginContactoUtilizador.text.toString().trim()
 
+            if (contacto.isBlank() || senha.isBlank()) {
+                Toast.makeText(this, "Preencha os campos para poder Entrar", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                val loginManager = LoginManager(this)
+                loginManager.fazerLogin(contacto, senha, object : LoginManager.LoginListener {
+                    override fun onLoginSuccess() {
+                        Toast.makeText(this@Login, "Bem-vindo", Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onUserNotExist() {
+                        Toast.makeText(this@Login, "Este usuário não existe", Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onLoginError(errorMessage: String) {
+                      Toast.makeText(this@Login,errorMessage,Toast.LENGTH_LONG).show()
+                    }
+                })
+
+            }
         }
     }
 }
